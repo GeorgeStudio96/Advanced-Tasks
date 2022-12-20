be28a3909eba26e36694fca71aad8951d6ab9b60
George Ershov <geo.erh96@gmail.com>
2022-12-20 09:38:14 +0300

    добавлено склонение, исправлена ошибка в месяце

--- js/lessons/lesson09.js
+++ js/lessons/lesson09.js
@@ -39,7 +39,11 @@ function getAllTimes() {
         let currentDay = date.getDate();
         let mounth = date.getMonth();
         let year = date.getFullYear();
+
         let hours = date.getHours();
+        if (hours < 10) {
+            hours = '0' + date.getHours()
+        }
         let minute = date.getMinutes();
         if (minute < 10) {
             minute = '0' + date.getMinutes();
@@ -56,9 +60,44 @@ function getAllTimes() {
             'часов'
         ]
 
+        let timeCorrectMinute = [
+            'минута',
+            'минуты',
+            'минут'
+        ]
+
+        let timeCorrectSecond = [
+            'секунда',
+            'секунды',
+            'секунд'
+        ]
+
+
+        let currentMin = timeCorrectMinute[0]
+
+        if (minute === 1 || minute === 21 || minute === 31 || minute === 41 || minute === 51) {
+            currentMin = timeCorrectMinute[0]
+        } else if (minute % 10 === 2 || (minute % 10 === 3) || (minute % 10 === 4)) {
+            currentMin = timeCorrectMinute[1]
+        } else {
+            currentMin = timeCorrectMinute[2]
+        }
+
+        let currentSec = timeCorrectSecond[0]
+
+        if (second === 1 || second === 21 || second === 31 || second === 41 || second === 51) {
+            currentSec = timeCorrectSecond[0] // секунда
+        } else if (second % 10 == 2 || second % 10 == 3 || second % 10 == 4) {
+            currentSec = timeCorrectSecond[1] // секунды
+        } else {
+            currentSec = timeCorrectSecond[2] // секунд
+        }
+
+
+
         let currentHourse = timeCorrect[0]
 
-        if (hours < 2 || hours === 21) {
+        if (hours === 1 || hours === 21) {
             currentHourse = timeCorrect[0] // час
         } else if ((hours % 10 === 2) || (hours % 10 === 3) || (hours % 10 === 4)) {
             currentHourse = timeCorrect[1] // часа
@@ -69,14 +108,14 @@ function getAllTimes() {
 
 
 
-        let result = 'Сегодня ' + days[day] + ' ' + currentDay + ' ' + mounths[mounth] + ` ${year} года` + '—' + hours + ' ' + currentHourse + ' ' + minute + ' минуты' + ' ' + second + ' секунды'
+        let result = 'Сегодня ' + days[day] + ' ' + currentDay + ' ' + mounths[mounth] + ` ${year} года` + '—' + hours + ' ' + currentHourse + ' ' + minute + ' ' + currentMin + ' ' + second + ' ' + currentSec
 
         return result
     }
 
     const currentDateMin = function () {
         let date = new Date();
-
+        console.log(date.getMonth());
         let currentDay = date.getDate();
         if (currentDay < 10) {
             currentDay = '0' + date.getDate()
@@ -107,7 +146,7 @@ function getAllTimes() {
 
 
 
-        let result = currentDay + '.' + currentMonth + '.' + currentYear + '—' + hours + ':' + minute + ':' + second
+        let result = currentDay + '.' + (currentMonth + 1) + '.' + currentYear + '—' + hours + ':' + minute + ':' + second
 
         return result
     }
@@ -123,6 +162,9 @@ function getAllTimes() {
 
 
 setInterval(getAllTimes, 1000)
+
+
+
 
 
 
